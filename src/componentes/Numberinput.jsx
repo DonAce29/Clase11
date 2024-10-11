0de38e3cd5fefa0b5ee8cde61ed88324426d2908
NICOLAS DONNEYS ALVARADO <nicolas.donneys@uao.edu.co>
2024-10-10 21:07:55 -0500

    cuarto

--- src/componentes/Numberinput.jsx
+++ src/componentes/Numberinput.jsx
@@ -1,30 +1,37 @@
-import React,{useState} from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import Resultado from './Resultado';
 import { operaciones } from '../helpers/operaciones';
 
-const Numberinput = () => {
-    const [numeros,setNumeros]= useState({
-        numero1:0,
-        numero2:0,
-    });
-    const{handleChange,suma,numero1,numero2}= operaciones(numeros,setNumeros);
-return (
-    <>
-    <label htmlFor="">
-        numero 1:
-         <input name='numero1'value={numero1} onChange={handleChange} type="number"/>
-    </label>
-    <label htmlFor="">
-        numero 2:  
-        <input name='numero2'value={numero2} onChange={handleChange}type="number"/>
-    </label>
-    <Resultado operacion={suma} calculo={suma()}/>
-        </>
-  )
-}
+const NumberInput = props => {
+  const [numeros, setnumeros] = useState({
+    numero1: 0,
+    numero2: 0,
+  });
 
-Numberinput.PropTypes={
-    name: PropTypes.string,
-}
-export default Numberinput
+  const { handleChange, suma, multiplicacion, division, resta, numero1, numero2 } = operaciones(numeros, setnumeros);
+
+  return (
+    <>
+      <label htmlFor="">Numero 1:
+        <input name='numero1' value={numero1} onChange={handleChange} type='number' />
+      </label>
+      <br />
+      <label htmlFor="">Numero 2:
+        <input name='numero2' value={numero2} onChange={handleChange} type='number' />
+      </label>
+      <br />
+      {/* Mostramos los resultados para cada operación */}
+      <Resultado operacion="Suma" calculo={suma()} />
+      <Resultado operacion="Multiplicación" calculo={multiplicacion()} />
+      <Resultado operacion="División" calculo={division()} />
+      <Resultado operacion="Resta" calculo={resta()} />
+    </>
+  );
+};
+
+NumberInput.propTypes = {
+  name: PropTypes.string,
+};
+
+export default NumberInput;
--- src/helpers/operaciones.js
+++ src/helpers/operaciones.js
@@ -1,13 +1,20 @@
-export const operaciones = (numeros,setNumeros)=>{
-    const{numero1,numero2}=numeros;
-    const handleChange=(e)=>{
-        setNumeros({
+export const operaciones = (numeros, setnumeros) => {
+    const { numero1, numero2 } = numeros;
+  
+    
+    const handleChange = (e) => {
+      setnumeros({
         ...numeros,
-        [e.target.name]:parseFloat([e.target.value])
-    });
-
+        [e.target.name]: parseFloat(e.target.value)
+      });
     }
+  
+    
     const suma = () => numero1 + numero2;
-    return{handleChange,suma,numero1,numero2}
-
-}
+    const multiplicacion = () => numero1 * numero2;
+    const division = () =>  numero1 / numero2; 
+    const resta = () => numero1 - numero2;
+  
+    
+    return { handleChange, suma, multiplicacion, division, resta, numero1, numero2 };
+  };
