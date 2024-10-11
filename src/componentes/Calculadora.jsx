bc054fe29522f4a00ad47e7f864c7d90333c6425
NICOLAS DONNEYS ALVARADO <nicolas.donneys@uao.edu.co>
2024-10-10 20:37:09 -0500

    segundo

---
+++ src/componentes/Calculadora.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Numberinput from "./Numberinput"
+const Calculadora = () => {
+  return (
+    <>
+    <Numberinput/>
+    </>
+  )
+}
+
+
+
+export default Calculadora
